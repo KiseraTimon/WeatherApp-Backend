1f1a2200610de6980f020e6ce04445a89ca26572
KiseraTimon <timonmarick@gmail.com>
2025-05-19 01:32:18 +0300

    #8 - Implementing Custom Error Handling

--- routes/api.php
+++ routes/api.php
@@ -5,62 +5,81 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
+# Importing Error Logging Mechaanism
+use App\Helpers\ErrorLogger;
+
 # Fetching Weather Details by Location
 Route::get('geocode', function (Request $request)
 {
-    # Capturing API Response
-    $response =
-
-        # Preparing API Request
-        Http::get(env('OPENWEATHER_GEOCODE_URL'), [
-            # Retrieving City From Query Params
-            'q' => $request->input('city'),
-
-            # Retrieving First Result From Query Results
-            'limit' => 1,
-
-            # Applying API Key
-            'appid' => env('OPENWEATHER_API_KEY')
-        ]);
-
-    # Handling Failed Requests
-    if($response->failed())
+    try
     {
-        return response()->json(["Weather data unavailable. Try again later"]);
-    }
+        # Capturing API Response
+        $response =
 
-    # Extracting Response as JSON
-    return $response->json();
+            # Preparing API Request
+            Http::get(env('OPENWEATHER_GEOCODE_URL'), [
+                # Retrieving City From Query Params
+                'q' => $request->input('city'),
+
+                # Retrieving First Result From Query Results
+                'limit' => 1,
+
+                # Applying API Key
+                'appid' => env('OPENWEATHER_API_KEY')
+            ]);
+
+        # Handling Failed Requests
+        if($response->failed())
+        {
+            return response()->json(["An Error Occured Retrieving Weather in Your Location. Try again later"]);
+        }
+
+        # Extracting Response as JSON
+        return $response->json();
+    }
+    catch (\Throwable $e)
+    {
+        ErrorLogger::handle($e);
+        return response()->json(["message" => "Geolocation Services Unavailable. Try Again Later."], 500);
+    }
 });
 
 # Default Weather Settings
 Route::get('/weather', function (Request $request)
 {
-    # Capturing API Response
-    $response =
-
-        #Preparing API Request
-        Http::get(env('OPENWEATHER_FORECAST_URL'), [
-            # Retrieving Latitude From Query Params
-            'lat' => $request->input('lat'),
-
-            # Retrieving Longitude From Query Params
-            'lon' => $request->input('lon'),
-
-            # Converting Temp Units to Celsius
-            'units'=> 'metric',
-
-            # Applying API Key
-            'appid' => env('OPENWEATHER_API_KEY')
-        ]);
-
-    # Handling Failed Requests
-    if($response->failed())
+    try
     {
-        return response()->json(["Weather data unavailable. Try again later"]);
-    }
+        # Capturing API Response
+        $response =
 
-    # Extracting Response as JSON
-    return $response->json();
+            #Preparing API Request
+            Http::get(env('OPENWEATHER_FORECAST_URL'), [
+                # Retrieving Latitude From Query Params
+                'lat' => $request->input('lat'),
+
+                # Retrieving Longitude From Query Params
+                'lon' => $request->input('lon'),
+
+                # Converting Temp Units to Celsius
+                'units'=> 'metric',
+
+                # Applying API Key
+                'appid' => env('OPENWEATHER_API_KEY')
+            ]);
+
+        # Handling Failed Requests
+        if($response->failed())
+        {
+            return response()->json(["An Error Occurred Retrieving Weather Details"]);
+        }
+
+        # Extracting Response as JSON
+        return $response->json();
+    }
+    catch (\Throwable $e)
+    {
+        ErrorLogger::handle($e);
+        return response()->json(['message'=> 'Weather Data Unavailable. Try Again Later'], 500);
+    }
 });
 ?>
