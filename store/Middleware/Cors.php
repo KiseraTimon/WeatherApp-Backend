4fb7e9e15db7e80be5cfaa40f9c283d293fdabe4
KiseraTimon <timonmarick@gmail.com>
2025-05-19 03:11:11 +0300

    #10 - Added Missing Middleware Codes

---
+++ store/Middleware/Cors.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        // If this is a preflight request, short-circuit and return the headers immediately.
+        if ($request->getMethod() === 'OPTIONS') {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', '*');
+        }
+
+        // Otherwise continue to the app and then add the header to the real response.
+        $response = $next($request);
+
+        return $response
+            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*');
+    }
+}
